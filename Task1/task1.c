6aee163628ae9d65cb05bd5e5261fc704b01b65a
Devendra Chhetri <devendramc6@gmail.com>
2026-07-07 07:03:40 +0000

    Created five worker threds

--- Task1/task1.c
+++ Task1/task1.c
@@ -29,10 +29,23 @@ void createProcesses()
     }
 }
 
+// Worker Thread Function
+void *worker(void *arg)
+{
+    int id = *(int *)arg;
+
+    printf("Thread-%d started.\n", id);
+
+    pthread_exit(NULL);
+}
+
 
 int main()
 {
     createProcesses();
+
+    pthread_t threads[NUM_THREADS];
+    int ids[NUM_THREADS];
 
     printf("Process Management and Threading\n\n");
 
@@ -43,5 +56,21 @@ int main()
         printf("Process-%d\n", processes[i]);
     }
 
+    printf("\nCreating Threads...\n");
+
+    for (int i = 0; i < NUM_THREADS; i++)
+    {
+        ids[i] = i + 1;
+
+        pthread_create(&threads[i],
+                       NULL,
+                       worker,
+                       &ids[i]);
+    }
+
+    for (int i = 0; i < NUM_THREADS; i++)
+    {
+        pthread_join(threads[i], NULL);
+    }
     return 0;
 }
